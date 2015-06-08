bb424f247b412b6e8804e17e26c2f9b0bed470e4
unknown <Anton_Lialiagin@EPRUSARW0718.saratov.epam.com>
2015-06-08 10:39:22 +0300

    add Yandex translate Api

---
+++ Addicto.YandexTranslateApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addicto.YandexTranslateApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "zahtevek";
+            var lang = "sl-ru";
+            var key = "trnsl.1.1.20150605T133357Z.0f99a7b0863cac2a.2e551c55751ee5ac41920a1a50eb2d42ba5ce9d6";
+            try
+            {
+                WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr/translate?" +
+                    "&key=" + key +
+                    "&text=" + text +
+                    "&lang=" + lang
+                    );
+
+                WebResponse response = request.GetResponse();   
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}",e.Message);
+            }
+        }
+    }
+}
